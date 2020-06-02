0e5b161cb11efb392fdf2968fe6f37d5ef52bab1
norisuke <norisuke.isn@gmail.com>
2020-06-02 18:11:00 +0900

    Regist models to DbContext

--- src/Infrastructure/Data/ApplicationDbContext.cs
+++ src/Infrastructure/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Infrastructure.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,6 +9,10 @@ namespace Infrastructure.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {}
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<TvProgram> TvPrograms { get; set; }
+        public DbSet<Corner> Corners { get; set; }
+        public DbSet<BaseSchedule> BaseSchedules { get; set; }
     }
 }
