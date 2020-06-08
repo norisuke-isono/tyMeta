a03f64143c09f041db0424676886d9974b1ea4bb
norisuke <norisuke.isn@gmail.com>
2020-06-09 01:23:18 +0900

    Add migration

---
+++ src/Infrastructure/Migrations/20200608162243_AddBroadcastAndAcheduleAndSpecification.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class AddBroadcastAndAcheduleAndSpecification : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Broadcasts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreateUserId = table.Column<string>(nullable: true),
+                    UpdateUserId = table.Column<string>(nullable: true),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: false),
+                    AirDate = table.Column<DateTime>(nullable: false),
+                    TvProgramId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Broadcasts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Broadcasts_TvPrograms_TvProgramId",
+                        column: x => x.TvProgramId,
+                        principalTable: "TvPrograms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Schedules",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreateUserId = table.Column<string>(nullable: true),
+                    UpdateUserId = table.Column<string>(nullable: true),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: false),
+                    Sequence = table.Column<int>(nullable: false),
+                    CornerId = table.Column<int>(nullable: true),
+                    BroadcastId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Schedules", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Schedules_Broadcasts_BroadcastId",
+                        column: x => x.BroadcastId,
+                        principalTable: "Broadcasts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Schedules_Corners_CornerId",
+                        column: x => x.CornerId,
+                        principalTable: "Corners",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Specifications",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreateUserId = table.Column<string>(nullable: true),
+                    UpdateUserId = table.Column<string>(nullable: true),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: false),
+                    Title = table.Column<string>(maxLength: 40, nullable: true),
+                    Text = table.Column<string>(maxLength: 1000, nullable: true),
+                    ScheduleId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Specifications", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Specifications_Schedules_ScheduleId",
+                        column: x => x.ScheduleId,
+                        principalTable: "Schedules",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Broadcasts_TvProgramId",
+                table: "Broadcasts",
+                column: "TvProgramId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Schedules_BroadcastId",
+                table: "Schedules",
+                column: "BroadcastId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Schedules_CornerId",
+                table: "Schedules",
+                column: "CornerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Specifications_ScheduleId",
+                table: "Specifications",
+                column: "ScheduleId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Specifications");
+
+            migrationBuilder.DropTable(
+                name: "Schedules");
+
+            migrationBuilder.DropTable(
+                name: "Broadcasts");
+        }
+    }
+}
