9c0afcd8345a7aebf1b69a85af2ff982503759a2
norisuke <norisuke.isn@gmail.com>
2020-06-09 12:01:37 +0900

    Schaffold Schdule pages

---
+++ src/Web/Pages/Schdules/Details.cshtml.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ApplicationCore.Entites;
+using Infrastructure.Data;
+
+namespace Web.Pages_Schdules
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Infrastructure.Data.ApplicationDbContext _context;
+
+        public DetailsModel(Infrastructure.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Schedule Schedule { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Schedule = await _context.Schedules
+                .Include(s => s.Broadcast)
+                .Include(s => s.Corner).FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Schedule == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
---
+++ src/Web/Pages/Schdules/Index.cshtml.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ApplicationCore.Entites;
+using Infrastructure.Data;
+
+namespace Web.Pages_Schdules
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Infrastructure.Data.ApplicationDbContext _context;
+
+        public IndexModel(Infrastructure.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Schedule> Schedule { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Schedule = await _context.Schedules
+                .Include(s => s.Broadcast)
+                .Include(s => s.Corner).ToListAsync();
+        }
+    }
+}
