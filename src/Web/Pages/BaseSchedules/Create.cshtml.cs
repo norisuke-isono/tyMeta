6f69c614dfc21e74756a10dccdcecfebad382829
norisuke <norisuke.isn@gmail.com>
2020-06-14 16:08:14 +0900

    Schaffold BaseSchedules pages

---
+++ src/Web/Pages/BaseSchedules/Create.cshtml.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ApplicationCore.Entites;
+using Infrastructure.Data;
+
+namespace Web.Pages.Schedules
+{
+    public class CreateModel : PageModel
+    {
+        private readonly Infrastructure.Data.ApplicationDbContext _context;
+
+        public CreateModel(Infrastructure.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["CornerId"] = new SelectList(_context.Corners, "Id", "Name");
+        ViewData["TvProgramId"] = new SelectList(_context.TvPrograms, "Id", "Name");
+            return Page();
+        }
+
+        [BindProperty]
+        public BaseSchedule BaseSchedule { get; set; }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.BaseSchedules.Add(BaseSchedule);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ src/Web/Pages/BaseSchedules/Delete.cshtml.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ApplicationCore.Entites;
+using Infrastructure.Data;
+
+namespace Web.Pages.Schedules
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly Infrastructure.Data.ApplicationDbContext _context;
+
+        public DeleteModel(Infrastructure.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public BaseSchedule BaseSchedule { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            BaseSchedule = await _context.BaseSchedules
+                .Include(b => b.Corner)
+                .Include(b => b.TvProgram).FirstOrDefaultAsync(m => m.Id == id);
+
+            if (BaseSchedule == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            BaseSchedule = await _context.BaseSchedules.FindAsync(id);
+
+            if (BaseSchedule != null)
+            {
+                _context.BaseSchedules.Remove(BaseSchedule);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
