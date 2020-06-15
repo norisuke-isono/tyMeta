44e0d2935bf8c4410f78e016ef762d084b702c1c
norisuke <norisuke.isn@gmail.com>
2020-06-15 17:30:41 +0900

    Scaffold Specification Edit page

---
+++ src/Web/Pages/Specification/Edit.cshtml.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ApplicationCore.Entites;
+using Infrastructure.Data;
+
+namespace Web.Pages_Specification
+{
+    public class EditModel : PageModel
+    {
+        private readonly Infrastructure.Data.ApplicationDbContext _context;
+
+        public EditModel(Infrastructure.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Specification Specification { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Specification = await _context.Specifications
+                .Include(s => s.Schedule).FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Specification == null)
+            {
+                return NotFound();
+            }
+           ViewData["ScheduleId"] = new SelectList(_context.Schedules, "Id", "Id");
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Specification).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SpecificationExists(Specification.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool SpecificationExists(int id)
+        {
+            return _context.Specifications.Any(e => e.Id == id);
+        }
+    }
+}
