c38dd4600977377a2656b8b894b7cdd2c1d6d3b5
norisuke <norisuke.isn@gmail.com>
2020-07-03 11:57:28 +0900

    Add Mappings tests

---
+++ tests/UnitTests/Web/Mappings/MappingTestsFixture.cs
@@ -0,0 +1,22 @@
+using AutoMapper;
+using Web.Mappings;
+
+namespace UnitTests.Mappings
+{
+    public class MappingTestsFixture
+    {
+        public MappingTestsFixture()
+        {
+            ConfigurationProvider = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MappingProfile>();
+            });
+
+            Mapper = ConfigurationProvider.CreateMapper();
+        }
+
+        public IConfigurationProvider ConfigurationProvider { get; }
+
+        public IMapper Mapper { get; }
+    }
+}
